9fe7c140ec8d4cac40a561db520c35f5ca723b37
Eva Viviani <evaviviani@Evas-MacBook-Pro.local>
2022-05-16 14:08:11 +0200

    script polished

--- speaker-segmentation/seg_pyannote.py
+++ speaker-segmentation/seg_pyannote.py
@@ -5,6 +5,7 @@ from rttm import RttmSeq, RttmObj
 import argparse
 import os
 
+
 parser = argparse.ArgumentParser(
     description="Detect speaker segments using pyannote.")
 parser.add_argument(
@@ -20,6 +21,20 @@ parser.add_argument(
     type=str,
     help="dataset on which speaker diarization is performed",
     dest="dataset"
+)
+parser.add_argument(
+    "--merge-threshold",
+    default="0.1",
+    type=float,
+    help="merges segments whose gap is less/equal than threshold",
+    dest="merge_th"
+)
+parser.add_argument(
+    "--filter-threshold",
+    default="0.5",
+    type=float,
+    help="removes segments whose duration is less than threshold",
+    dest="filter_th"
 )
 parser.add_argument(
     "--files-is-list",
@@ -58,63 +73,33 @@ if args.files_is_range:
 else:
     file_indices = args.files
 
-""" commented out
-for i, sample in enumerate(data):
-    if i in file_indices:
-        filename = sample["file"]
-        result = pipeline(filename)
-        segments = [RttmObj(
-            type="SPEAKER",
-            file=filename,
-            chnl=1,
-            tbeg=float(seg.start),
-            tdur=float(seg.end - seg.start)
-        ) for seg in result.itersegments()]
-        RttmSeq(segments).write(os.path.join(args.base_dir, "results",
-                                             "pyannote", f"pa_ami_micro_test_sample_{i}.rttm"))
-        print(f"Processed sample {i}: {filename}")
-"""
-
-# 13/05/2022 latest edit
 
 # function to filter out segments whose duration is below a threshold
-def duration_selection(segments, threshold):
+def remove_short_segments(segments, threshold):
     segments_filtered = [seg for seg in segments
        if seg.tdur > threshold] 
     return segments_filtered
 
 # function to merge segments whose intersegment interval is below a threshold
-def merge_segments(segments, threshold, include_gaps = True):
+def merge_segments(segments, threshold):
     merged_segments = [segments[0]]
     for index, segment in enumerate(segments[1:]):
         end = (merged_segments[-1].tbeg + merged_segments[-1].tdur) # end of first segment
-        gap_dur = (segment.tbeg - end)
+        gap_dur = (segment.tbeg - end) #gap duration
         merge = gap_dur <= threshold
         if merge:
-            if include_gaps:  
-                joint_segment = RttmObj(
-                    type=segment.type,
-                    file=segment.file,
-                    chnl=segment.chnl,
-                    tbeg=float(merged_segments[-1].tbeg), #beginning of fist segment
-                    tdur=float(merged_segments[-1].tdur + gap_dur + segment.tdur) #duration 1st segment + gap + duration 2nd segment
-                )
-            else:
-                joint_segment = RttmObj(
-                    type=segment.type,
-                    file=segment.file,
-                    chnl=segment.chnl,
-                    tbeg=float(merged_segments[-1].tbeg), #beginning of fist segment
-                    tdur=float(merged_segments[-1].tdur + segment.tdur) #duration 1st segment + duration 2nd segment
-                )
+            joint_segment = RttmObj(
+                type=segment.type,
+                file=segment.file,
+                chnl=segment.chnl,
+                tbeg=float(merged_segments[-1].tbeg), #beginning of first segment
+                tdur=float(merged_segments[-1].tdur + gap_dur + segment.tdur) #duration 1st segment + gap + duration 2nd segment
+            )
             merged_segments[-1] = joint_segment
         else:
             merged_segments.append(segment)
     return merged_segments
 
-# threshold arguments
-filter_th = 0.5 # segment duration
-merge_th = 0.1 # segment gap 
 
 for i, sample in enumerate(data):
     if i in file_indices:
@@ -128,11 +113,8 @@ for i, sample in enumerate(data):
             tdur=float(seg.end - seg.start)
         ) for seg in result.itersegments()]
         
-        merged_segments = merge_segments(segments, merge_th) # merge close segments first
-        filtered_segments = duration_selection(merged_segments, filter_th) # then remove short segments
+        merged_segments = merge_segments(segments, args.merge_th) # merge close segments first
+        filtered_segments = remove_short_segments(merged_segments, args.filter_th) # then remove short segments
         RttmSeq(filtered_segments).write(os.path.join(args.base_dir, "results",
                                              "pyannote", f"pa_ami_micro_test_sample_{i}.rttm"))
         print(f"Processed sample {i}: {filename}")
-    
-
-        
