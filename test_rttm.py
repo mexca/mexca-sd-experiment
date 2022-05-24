34a404a9c57a87350c7c5cce52e884c220017d77
Malte Lüken <malte_lueken@arcor.de>
2022-05-24 17:30:25 +0200

    Add notebook to create DED21 dataset

--- test_rttm.py
+++ test_rttm.py
@@ -1,4 +1,8 @@
 from click import FileError
+<<<<<<< Updated upstream
+=======
+from copy import copy, deepcopy
+>>>>>>> Stashed changes
 from rttm import RttmSeq, RttmObj, get_default_header, read_rttm
 import pytest
 
@@ -70,6 +74,22 @@ class TestRttmObj:
                 conf = 1.4
             )
 
+<<<<<<< Updated upstream
+=======
+    
+    def test_copy(self):
+        obj = RttmObj(
+            type = "",
+            file = "",
+            chnl = 1,
+            tbeg = 1.0,
+            tdur = 1.0
+        )
+        obj_copy = copy(obj)
+
+        assert obj is not obj_copy
+
+>>>>>>> Stashed changes
 
 class TestRttmSeq:
     def test_init(self):
@@ -93,6 +113,78 @@ class TestRttmSeq:
         assert hasattr(obj, "sequence")
 
 
+<<<<<<< Updated upstream
+=======
+    def test_copy(self):
+        seq = [
+            RttmObj(
+                type = "",
+                file = "",
+                chnl = 1,
+                tbeg = 1.0,
+                tdur = 1.0
+            ),
+            RttmObj(
+                type = "",
+                file = "",
+                chnl = 1,
+                tbeg = 1.0,
+                tdur = 1.0
+            )
+        ]
+        obj = RttmSeq(seq)
+        obj_copy = copy(obj)
+
+        assert obj is not obj_copy
+
+
+    def test_deepcopy(self):
+        seq = [
+            RttmObj(
+                type = "",
+                file = "",
+                chnl = 1,
+                tbeg = 1.0,
+                tdur = 1.0
+            ),
+            RttmObj(
+                type = "",
+                file = "",
+                chnl = 1,
+                tbeg = 1.0,
+                tdur = 1.0
+            )
+        ]
+        obj = RttmSeq(seq)
+        obj_copy = deepcopy(obj)
+
+        assert obj.sequence[0] is not obj_copy.sequence[0]
+
+
+    def test_sort(self):
+        seq = [
+            RttmObj(
+                type = "",
+                file = "",
+                chnl = 1,
+                tbeg = 2.0,
+                tdur = 1.0
+            ),
+            RttmObj(
+                type = "",
+                file = "",
+                chnl = 1,
+                tbeg = 0.0,
+                tdur = 1.0
+            )
+        ]
+        obj = RttmSeq(seq)
+        obj_sorted = obj.sort()
+        
+        assert obj_sorted.sequence[0].tbeg < obj_sorted.sequence[1].tbeg
+
+
+>>>>>>> Stashed changes
     def test_read_write(self):
         test_filename = "test.rttm"
         seq = [
